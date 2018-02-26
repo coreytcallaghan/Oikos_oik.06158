270187ac0bf503db08c35630d536d3a17d12b08d
wcornwell <wcornwell@gmail.com>
2018-02-26 17:10:19 +1100

    syncing phylo and traits

--- R/phylo_fxns.R
+++ R/phylo_fxns.R
@@ -1,15 +1,39 @@
 read_bird_tree<-function(){
   require(ape)
-  dir.create("Data/phylo/", showWarnings = FALSE)
+  if(!exists("Data/phylo/")) dir.create("Data/phylo/", showWarnings = FALSE)
   download.file("https://www.dropbox.com/s/gfackoogv6n0bv0/AllBirdsEricson1.tre?raw=1","Data/phylo/phy.tre")
-  bird_tree<-read.tree(file="Data/phylo/phy.tre")[[1]]
+  bird_trees<-ape::read.tree(file="Data/phylo/phy.tre")[[1]]
   #drop tips to get down to Aus species here
-  return(bird_tree)
+  return(bird_trees)
 }
 
 
-plot_bird_tree<-function(bird_tree){
+plot_bird_tree<-function(aus_bird_tree){
   pdf("figures/bird_plot.pdf")
-  plot(bird_tree,type="r",show.tip.label = FALSE)
+  plot(aus_bird_tree,type="r",show.tip.label = TRUE,cex=0.4,show.node.label=TRUE)
   dev.off()
 }
+
+
+subset_tree<-function(trees,traits){
+  non_aus_sp<-bird_trees$tip.label[!bird_trees$tip.label %in% traits$binom]
+  aus_bird_tree<-drop.tip(bird_trees,non_aus_sp)
+  return(aus_bird_tree)
+  }
+
+
+plot_bird_tree_traits<-function(aus_bird_tree,traits){
+  trait<-as.array(traits$mean_body_size)
+  row.names(trait)<-row.names(traits)
+  trait<-subset(trait,trait!="NaN")
+  trait<-subset(trait,names(trait)%in%aus_bird_tree$tip.label)
+  tree_plotting<-drop.tip(aus_bird_tree,aus_bird_tree$tip.label[!aus_bird_tree$tip.label%in%row.names(trait)])
+  
+  obj <- contMap(tree_plotting, log10(trait), fsize = c(0.6, 1), outline = FALSE)
+  
+  pdf("figures/bird_plot_traits.pdf")
+  plotTree.wBars(obj$tree, log10(trait), method = "plotSimmap", colors = obj$cols,
+                 type = "fan", scale = 5)
+  dev.off()
+}
+
---
+++ R/trait_fxns.R
@@ -0,0 +1,19 @@
+read_trait_data_in<-function(){
+  require(readr)
+traits<-read_csv("Data/Raw trait data/Australian_Bird_Data_Version_1.csv")
+         traits$binom<- paste(traits$`4_Genus_name_2`, traits$`5_Species_name_2`,sep="_")
+         return(traits)
+}
+
+
+read_process_trait_data<-function(){
+  traits<-read_trait_data_in()
+  traits %>%
+    mutate(body_mass=ifelse(`99_Body_mass_average_8`=="NAV",NA,as.numeric(`99_Body_mass_average_8`))) %>%
+    group_by(binom) %>%
+    summarise(mean_body_size=mean(body_mass,na.rm=T)) -> ms
+              
+    ms<-data.frame(ms)
+   row.names(ms)<-ms$binom
+  return(ms)  
+}
