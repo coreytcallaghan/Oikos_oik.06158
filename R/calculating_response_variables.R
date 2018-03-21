e58e920e2bc696dec90ea56f395073316c30e3fb
Corey Callaghan <callaghan.corey.t@gmail.com>
2018-03-21 16:24:39 +1100

    calculating response variables and plotting the distribution

---
+++ R/calculating_response_variables.R
@@ -0,0 +1,47 @@
+### Calculating response variables for each species
+### This script is used to create the response variables for all species included in the analysis
+
+
+### load eBird data
+load_ebird_data <- function() {
+  load('Data/eBird data/Final eBird data for analysis/species_urban.RData')
+  species_urban
+}
+
+### calculate response variable df
+response_variables <- function() {
+  
+  response_variables <- species_urban %>%
+  group_by(COMMON_NAME) %>%
+  summarise(adapter= quantile(log(avg_rad), .90),
+            exploiter = quantile(log(avg_rad), .50),
+            avoider = quantile(log(avg_rad), .10),
+            N = n(),
+            unique_localities = length(unique(LOCALITY_ID)))
+  
+  response_variables
+  
+}
+
+
+### Plot the histogram of the response variables
+distribution_response_variables <- function(response_variables) {
+  
+  pdf("figures/distribution_response_variables.pdf")
+  
+  print(response_variables %>%
+    gather(key = "response_level", value = "value", adapter:avoider) %>%
+    ggplot(., aes(value))+
+    geom_histogram(bins = 50, 
+                   col="black", 
+                   fill="green", 
+                   alpha = .2)+
+    theme_bw()+
+    xlab("Value")+
+    ylab("Count")+
+    facet_wrap(~response_level, scales="free"))
+  
+  dev.off()
+}
+
+
