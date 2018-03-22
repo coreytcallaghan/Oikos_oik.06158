311a18af7219fdd589cdb47e7f21e00a9bbc4f43
Corey Callaghan <callaghan.corey.t@gmail.com>
2018-03-22 13:35:44 +1100

    adding more traits

--- R/trait_fxns.R
+++ R/trait_fxns.R
@@ -5,17 +5,32 @@ traits<-read_csv("Data/Raw trait data/Australian_Bird_Data_Version_1.csv")
          return(traits)
 }
 
+Mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+
 
 read_process_trait_data<-function(){
   traits<-read_trait_data_in()
-  traits %>%
+    traits %>%
     mutate(body_mass=ifelse(`99_Body_mass_average_8`=="NAV",NA,as.numeric(`99_Body_mass_average_8`))) %>%
     mutate(clutch_size=ifelse(`178_Clutch_size_average_12`=="NAV",NA,as.numeric(`178_Clutch_size_average_12`))) %>%
     mutate(category=`29_Population_description_4`) %>%
     mutate(status=`49_Australian_status_July_2015_5`) %>%
     mutate(brain_size=ifelse(`113_Brain_mass_8`=="NAV", NA, as.numeric(`113_Brain_mass_8`))) %>%
+    mutate(feeding_habitat_generalism=rowSums(.[115:145], na.rm=TRUE)) %>%
+    mutate(breeding_generalism=rowSums(.[146:162], na.rm=TRUE)) %>%
+    mutate(diet_generalism=rowSums(.[163:173], na.rm=TRUE))
     group_by(binom) %>%
-    summarise(mean_body_size=mean(body_mass,na.rm=T),clutch_size=mean(clutch_size,na.rm=T)) -> ms
+    summarise(mean_body_size=mean(body_mass,na.rm=T), 
+              clutch_size=mean(clutch_size,na.rm=T),
+              category=Mode(category),
+              status=Mode(status),
+              brain_size=mean(brain_size, na.rm=T),
+              feeding_habitat_generalism=mean(feeding_habitat_generalism),
+              breeding_habitat_generalism=mean(breeding_habitat_generalism),
+              diet_generalism=mean(diet_generalism)) -> ms
               
     ms<-data.frame(ms)
    row.names(ms)<-ms$binom
