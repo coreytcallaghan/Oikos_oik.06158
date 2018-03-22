1050faecb66fc9b7035349731191d6f33b0a444b
Corey Callaghan <callaghan.corey.t@gmail.com>
2018-03-22 17:53:36 +1100

    added plots of histograms in addition to the density plots

--- R/density_plot_per_species.R
+++ R/density_plot_per_species.R
@@ -26,6 +26,25 @@ avg_rad_density_for_each_species <- function (species_urban) {
   
   dev.off()
   
+  pdf("figures/histogram_plot_per_species.pdf")
+  
+  for (i in 1:n_pages) {
+    
+    print(ggplot(species_urban, aes(avg_rad)) +
+            geom_histogram(bins = 50, 
+                           col="black", 
+                           fill="green", 
+                           alpha = .2) + 
+            scale_x_log10() + 
+            theme_classic() +
+            xlab("Average radiance") +
+            ylab("Count") +
+            facet_wrap_paginate(~COMMON_NAME, ncol = 4, nrow = 5, scales="free_y", page = i))
+    
+  }
+  
+  dev.off()
+  
   
 }
 
