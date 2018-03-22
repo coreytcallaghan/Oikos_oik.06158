003e080620b779c4b2a9c98557dbe48333e3b428
Corey Callaghan <callaghan.corey.t@gmail.com>
2018-03-22 13:23:09 +1100

    added a few more traits

--- R/trait_fxns.R
+++ R/trait_fxns.R
@@ -11,6 +11,9 @@ read_process_trait_data<-function(){
   traits %>%
     mutate(body_mass=ifelse(`99_Body_mass_average_8`=="NAV",NA,as.numeric(`99_Body_mass_average_8`))) %>%
     mutate(clutch_size=ifelse(`178_Clutch_size_average_12`=="NAV",NA,as.numeric(`178_Clutch_size_average_12`))) %>%
+    mutate(category=`29_Population_description_4`) %>%
+    mutate(status=`49_Australian_status_July_2015_5`) %>%
+    mutate(brain_size=ifelse(`113_Brain_mass_8`=="NAV", NA, as.numeric(`113_Brain_mass_8`))) %>%
     group_by(binom) %>%
     summarise(mean_body_size=mean(body_mass,na.rm=T),clutch_size=mean(clutch_size,na.rm=T)) -> ms
               
