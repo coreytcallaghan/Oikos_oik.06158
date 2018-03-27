9b029fcb28f22ea30dbf626428018447c8a65285
Corey Callaghan <callaghan.corey.t@gmail.com>
2018-03-28 07:28:56 +1100

    no log

--- scripts/mixtools_means.R
+++ scripts/mixtools_means.R
@@ -13,11 +13,11 @@ test_species <- c("Common Myna", "Australian Ibis", "Southern Emuwren", "Noisy M
 species_urban %>% group_by(COMMON_NAME) %>% 
   summarise(
     nobs = n(),
-    log_mean = log(mean(avg_rad))
-  ) %>% arrange(desc(log_mean)) -> sp_summary
+    mean = mean(avg_rad)
+  ) %>% arrange(desc(mean)) -> sp_summary
 
 sp_summary %>% 
-  ggplot(aes(log_mean)) + geom_histogram()
+  ggplot(aes(mean)) + geom_histogram()
 
 sp_summary$mean %>% log %>% density(bw = 0.2) %>% plot
 mixtools::normalmixEM(sp_summary$mean %>% log, k = 3) -> mxdist3
