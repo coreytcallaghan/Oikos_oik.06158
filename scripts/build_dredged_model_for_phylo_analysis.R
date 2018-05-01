70e191da09c8210b929f0e57e09569d43fc22497
Corey Callaghan <callaghan.corey.t@gmail.com>
2018-05-02 09:10:42 +1000

    need rescale function loaded to clusters in order to get models to work

--- scripts/build_dredged_model_for_phylo_analysis.R
+++ scripts/build_dredged_model_for_phylo_analysis.R
@@ -5,6 +5,37 @@
 ## done outside of the remake workflow as 
 ## the environment issue was very difficult to navigate!
 
+## arm package is really tricky and won't be recognized to load
+## into the cluster export. A similar issue to what we had in the
+## remake workflow. Here, I pulled the arm::rescale function out and put
+## it into the environment and then load it onto the cluserexport function
+rescale <- function (x, binary.inputs = "center") 
+{
+  if (!is.numeric(x)) {
+    x <- as.numeric(factor(x))
+    x.obs <- x[!is.na(x)]
+  }
+  x.obs <- x[!is.na(x)]
+  if (length(unique(x.obs)) == 2) {
+    if (binary.inputs == "0/1") {
+      x <- (x - min(x.obs))/(max(x.obs) - min(x.obs))
+      return(x)
+    }
+    else if (binary.inputs == "-0.5,0.5") {
+      return(x - 0.5)
+    }
+    else if (binary.inputs == "center") {
+      return(x - mean(x.obs))
+    }
+    else if (binary.inputs == "full") {
+      return((x - mean(x.obs))/(2 * sd(x.obs)))
+    }
+  }
+  else {
+    return((x - mean(x.obs))/(2 * sd(x.obs)))
+  }
+}
+
 build_dredged_model <- function() {
   remake::dump_environment()
   
@@ -12,7 +43,7 @@ build_dredged_model <- function() {
   clusterType <- if(length(find.package("snow", quiet = TRUE))) "SOCK" else "PSOCK"
   clust <- try(makeCluster(getOption("cl.cores", 10), type = clusterType))
   
-  clusterExport(clust, c("analysis_data", "phylolm", "aus_bird_tree"), envir = environment())
+  clusterExport(clust, c("analysis_data", "phylolm", "rescale", "aus_bird_tree"), envir = .GlobalEnv)
   
   model.set <- pdredge(phy_mod_rescaled, m.lim=c(0, 11), cluster=clust)
   
